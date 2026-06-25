4ad17889d406bb352850867f11b1f41f471624da
Siddharth Rai <135386624+Sidd-Rai@users.noreply.github.com>
2026-06-26 00:29:08 +0530

    Added about page

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
-
 def render():
     st.title("About")
     
     st.markdown("""
-   #### Work in progress!!
-    ~Sid
+    **Resume Reviewer Pro** is an open-source, multi-agent resume analysis tool built to move beyond single-prompt AI wrappers. 
+    
+    Check out the links below to see the code, read about the architecture, or join the discussion:
+    
+    * 📖 **Medium Article:** [How I Built a Multi-Agent Resume Critic](https://medium.com/@siddharthrai2468/how-i-built-a-multi-agent-resume-critic-and-why-single-prompts-fail-85797da4477d)
+    * 💻 **GitHub Repo & README:** [Sidd-Rai/ResumeReviewerPro](https://github.com/Sidd-Rai/ResumeReviewerPro)
+    ---
+    
+    **Siddharth**
+    📧 siddharthrai2468@gmail.com  
     """)
